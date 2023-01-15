37b12bb923d24be29b2f35ac8a993cdaad4e2fbe
Jasmin <ssongnari@gmail.com>
2023-01-15 15:47:38 -0500

    commit tree 5ab32553a22544e7fea73873e7d71d0040f7c7cc

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,12 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def give_freebie(dev, item_name, value)
+        Freebie.create(company: name, dev: dev.name)
+    end
+
+    def oldest_company
+        self.founding_year.minimum
+    end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,4 @@
 class Dev < ActiveRecord::Base
-
+    has_many :freebies
+    has_many :company, through: :freebies
 end
